9e2ba1a6d070c2af008903b355029eb037e828c0
rafael <rcf@DELL-RAFA.localdomain>
2022-12-30 19:54:24 -0300

    first commit

---
+++ node/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = process.env.APP_PORT || 3000
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+}
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(config)
+
+app.get('/', (req, res) => {
+
+  connection.query(`INSERT INTO people (nome) VALUES ('Rafael Fernandes')`)
+
+  connection.query(`SELECT nome FROM people`, (error, results, fields) => {
+    res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map(el => `<li>${el.nome}</li>`).join('') : ''}
+      </ol>
+    `)
+  })
+})
+
+app.listen(port, () => {
+  console.log('Rodando na porta ', port);
+})
